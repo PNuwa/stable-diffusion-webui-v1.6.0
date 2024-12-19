f2b72e1bf474d78f9dd66684adf379047e476f4d
nyan9 <2575044704@qq.com>
2024-12-19 13:11:59 +0800

    Update webui.py

--- webui.py
+++ webui.py
@@ -95,10 +95,6 @@ def webui():
         print('部署成功，记得打赏作者 Nyan9哦')
         print('部署成功，记得打赏作者 Nyan9哦')
         print('部署成功，记得打赏作者 Nyan9哦')
-        print('肚子好饿饿！！')
-        print('求求哥哥姐姐们打赏，上天请给我赏口饭吃吧！！！')
-        print('已经吃不起饭了！！！！')
-        print('快要饿死在家里了！！！！！！！（哭）')
         app, local_url, share_url = shared.demo.launch(
             share=cmd_opts.share,
             server_name=initialize_util.gradio_server_name(),
